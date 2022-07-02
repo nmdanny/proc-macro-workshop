b184df2739118c524f23000372dd817a743e82f9
nmdanny <nmdanny@gmail.com>
2022-07-02 14:13:37 +0300

    Refactor p6 to use visitor

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,56 @@
 use proc_macro::TokenStream;
-use quote::{quote, format_ident, ToTokens};
-use syn::{parse_macro_input, DeriveInput, Data, parse_quote, Field, Type, Path, TypePath, Ident, AngleBracketedGenericArguments, GenericArgument};
-
+use quote::{format_ident, quote, ToTokens};
+use syn::{
+    parse_macro_input, parse_quote, visit::Visit, AngleBracketedGenericArguments, Data,
+    DeriveInput, Field, GenericArgument, Ident, Path, Type, TypePath,
+};
 
 struct BuilderField<'f> {
-    field: &'f Field
+    field: &'f Field,
 }
 
-impl <'f> BuilderField<'f> {
+fn find_underlying<'ast>(ty: &'ast Type, outer_name: &'ast str) -> Option<&'ast Type> {
+    struct Visitor<'ast> {
+        inside_outer: bool,
+        outer_name: &'ast str,
+        underlying: Option<&'ast Type>,
+    }
+
+    impl<'ast> syn::visit::Visit<'ast> for Visitor<'ast> {
+        fn visit_type(&mut self, i: &'ast Type) {
+            if let Type::Path(TypePath { path, .. }) = i {
+                self.inside_outer = path.segments.first().map(|s| s.ident.to_string())
+                    == Some(self.outer_name.into())
+            }
+            if self.underlying.is_none() {
+                syn::visit::visit_type(self, i);
+            }
+        }
+
+        fn visit_path_arguments(&mut self, i: &'ast syn::PathArguments) {
+            match i {
+                syn::PathArguments::AngleBracketed(ab)
+                    if self.inside_outer && self.underlying.is_none() =>
+                {
+                    let first_arg = ab.args.first();
+                    if let Some(GenericArgument::Type(ty)) = first_arg {
+                        self.underlying = Some(ty);
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+    let mut visitor = Visitor {
+        inside_outer: false,
+        outer_name,
+        underlying: None,
+    };
+    visitor.visit_type(ty);
+    visitor.underlying
+}
+
+impl<'f> BuilderField<'f> {
     fn new(field: &'f Field) -> Self {
         BuilderField { field }
     }
@@ -26,42 +69,13 @@ impl <'f> BuilderField<'f> {
 
     fn option_of(&self) -> Option<&syn::Type> {
         let ty = &self.field.ty;
-        match ty {
-            Type::Path(TypePath {
-                qself: None,
-                path: Path {
-                    segments,
-                    ..
-                }
-            }) => {
-                let first_segment = segments.first()?;
-                (first_segment.ident.to_string() == "Option").then_some(())?;
-                match &first_segment.arguments {
-                    syn::PathArguments::AngleBracketed(
-                        AngleBracketedGenericArguments {
-                            args,
-                            ..
-                        }
-                    ) => {
-                        let first_arg = args.first().expect("Option should have one type argument");
-                        if let GenericArgument::Type(ty) = first_arg {
-                            return Some(ty);
-                        }
-                    }
-                    _ => panic!("Expected option to be angle bracketed")
-                }
-
-            },
-            _ => return None,
-        }
-        None
+        find_underlying(ty, "Option")
     }
-
 
     fn to_field(&self) -> Field {
         let mut new_field = self.field.clone();
         if self.is_option() {
-            return new_field
+            return new_field;
         }
         let ty = self.ty();
         new_field.ty = parse_quote! {
@@ -75,7 +89,7 @@ impl <'f> BuilderField<'f> {
         let ty = self.ty();
 
         if let Some(underlying_ty) = self.option_of() {
-            quote ! {
+            quote! {
                 pub fn #ident(&mut self, #ident: #underlying_ty) -> &mut Self {
                     self.#ident = Some(#ident);
                     self
@@ -89,9 +103,7 @@ impl <'f> BuilderField<'f> {
                 }
             }
         }
-
     }
-
 
     fn to_build_body(&self) -> impl ToTokens {
         let ident = self.ident();
@@ -106,7 +118,6 @@ impl <'f> BuilderField<'f> {
                 let #ident = self.#ident.take().ok_or(#err_msg)?;
             }
         }
-
     }
 
     fn to_init_none(&self) -> impl ToTokens {
@@ -122,15 +133,11 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_name = format_ident!("{}Builder", input.ident);
 
     let fields = match &input.data {
-        Data::Struct(ref s) => s.fields
-            .iter()
-            .map(BuilderField::new)
-            .collect::<Vec<_>>(),
-        _ => panic!("Expected struct")
+        Data::Struct(ref s) => s.fields.iter().map(BuilderField::new).collect::<Vec<_>>(),
+        _ => panic!("Expected struct"),
     };
 
     let builder_fields = fields.iter().map(BuilderField::to_field);
-
 
     let builder_init_nones = fields.iter().map(BuilderField::to_init_none);
     let builder_setters = fields.iter().map(BuilderField::to_setter);
